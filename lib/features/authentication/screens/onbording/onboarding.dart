7de374e5c0392f6f132b186552586a04f7d27944
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-07-31 18:51:04 +0500

    seprate onboarding page

--- lib/features/authentication/screens/onbording/onboarding.dart
+++ lib/features/authentication/screens/onbording/onboarding.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:shopping_app/utils/constant/images.dart';
+import 'package:shopping_app/utils/constant/sizes.dart';
 import 'package:shopping_app/utils/constant/text.dart';
+
+import '../../../../utils/helpers/device_helper.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -12,19 +15,41 @@ class OnboardingScreen extends StatelessWidget {
       body: Stack(
         children: [
           PageView(
-           children: [
-             Padding(
-               padding: const EdgeInsets.only(left: ,right: ,top:),
-               child: Column(
-                 children: [
-                   Lottie.asset(MyImages.onboarding1Animation),
-                   Text(MyText.onboardingTitle1,style: Theme.of(context).textTheme.headlineMedium,),
-                   Text(MyText.onboardingSubTitle1,textAlign: TextAlign.center,),
-                 ],
-               ),
-             ),
+            children: [
+              OnBoardingPage(),
+              OnBoardingPage(),
+              OnBoardingPage(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-           ],
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: MySizes.defaultSpace,
+        right: MySizes.defaultSpace,
+        top: MyDeviceHelper.getAppBarHeight(),
+      ),
+      child: Column(
+        children: [
+          Lottie.asset(MyImages.onboarding1Animation),
+          Text(
+            MyText.onboardingTitle1,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Text(
+            MyText.onboardingSubTitle1,
+            textAlign: TextAlign.center,
           ),
         ],
       ),
