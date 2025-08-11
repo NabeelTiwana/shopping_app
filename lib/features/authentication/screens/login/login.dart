3984d770f8616ecfd4195f455b439dc550f9cdf2
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-08-11 17:28:23 +0500

    create login screen

---
+++ lib/features/authentication/screens/login/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          //Header
+          //Form
+          //divider
+          //social button
+
+        ],
+      ),
+    );
+  }
+}
