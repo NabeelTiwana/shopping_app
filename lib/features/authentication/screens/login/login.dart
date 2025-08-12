af0c35982b2de32dcede42b7cfbda666f72440bd
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-08-12 11:52:51 +0500

    crate login header

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shopping_app/common/style/padding.dart';
 import 'package:shopping_app/common/widget/button/elevated_button.dart';
+import 'package:shopping_app/features/authentication/screens/login/widgets/login_header.dart';
 import 'package:shopping_app/utils/constant/colors.dart';
+import 'package:shopping_app/utils/constant/images.dart';
 import 'package:shopping_app/utils/constant/sizes.dart';
 import 'package:shopping_app/utils/constant/text.dart';
+import 'package:shopping_app/utils/helpers/helper_functions.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark=MyHelperFunctions.isDarkMode(context);
     return Scaffold(
       body: Padding(
         padding: MyPadding.screenPadding,
@@ -20,33 +25,17 @@ class LoginScreen extends StatelessWidget {
           children: [
             //Header
             //Title& subTitle
-            LoginHeader(),
+            MyLoginHeader(),
             SizedBox(height: MySizes.spaceBtwSections),
             //Form
-            LoginForm(),
+            MyLoginForm(),
             SizedBox(height: MySizes.spaceBtwSections),
             //divider
-           Row(
-             children: [
-               Expanded(child: Divider(indent: 60,endIndent: 5,thickness: 0.5,)),
-               Text(MyText.orSignInWith,style: Theme.of(context).textTheme.labelMedium,),
-               Expanded(child: Divider(indent: 5,endIndent: 60,thickness: 0.5,)),
-             ],
-           ),
-            SizedBox(height:MySizes.spaceBtwSections ,),
+            MyFormDivider(dark: dark),
+            SizedBox(height: MySizes.spaceBtwSections),
             //Footer
             //social button
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: MyColor.grey),
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  child: IconButton(onPressed: (){}, icon: ),
-                ),
-              ],
-            ),
+            MySocialButton(),
           ],
         ),
       ),
@@ -54,10 +43,79 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-class LoginForm extends StatelessWidget {
-  const LoginForm({
+class MySocialButton extends StatelessWidget {
+  const MySocialButton({
     super.key,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: MyColor.grey),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              MyImages.googleIcon,
+              height: MySizes.iconMd,
+              width: MySizes.iconMd,
+            ),
+          ),
+        ),
+        SizedBox(width: MySizes.spaceBtwItems,),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: MyColor.grey),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              MyImages.facebookIcon,
+              height: MySizes.iconMd,
+              width: MySizes.iconMd,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyFormDivider extends StatelessWidget {
+  const MyFormDivider({
+    super.key,
+    required this.dark,
+  });
+
+  final bool dark;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Divider(indent: 60, endIndent: 5, thickness: 0.5,color: dark?MyColor.darkGrey:MyColor.grey,),
+        ),
+        Text(
+          MyText.orSignInWith,
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
+        Expanded(
+          child: Divider(indent: 5, endIndent: 60, thickness: 0.5,color: dark?MyColor.darkGrey:MyColor.grey,),
+        ),
+      ],
+    );
+  }
+}
+
+class MyLoginForm extends StatelessWidget {
+  const MyLoginForm({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -80,7 +138,7 @@ class LoginForm extends StatelessWidget {
             suffixIcon: Icon(Iconsax.eye),
           ),
         ),
-        SizedBox(height: MySizes.spaceBtwInputFields/2,),
+        SizedBox(height: MySizes.spaceBtwInputFields / 2),
         //Remember Me & forgetPassword
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -92,40 +150,18 @@ class LoginForm extends StatelessWidget {
               ],
             ),
             //forgetPassword
-            TextButton(onPressed: (){}, child: Text(MyText.forgetPassword)),
+            TextButton(onPressed: () {}, child: Text(MyText.forgetPassword)),
           ],
         ),
-        SizedBox(height: MySizes.spaceBtwSections,),
+        SizedBox(height: MySizes.spaceBtwSections),
         //Sign in
-        MyElevatedButton(onPressed: (){}, child: Text(MyText.signIn)),
-         SizedBox(height: MySizes.spaceBtwItems/2,),
+        MyElevatedButton(onPressed: () {}, child: Text(MyText.signIn)),
+        SizedBox(height: MySizes.spaceBtwItems / 2),
         //Create Account
-        MyElevatedButton(onPressed: (){}, child: Text(MyText.createAccount)),
+        MyElevatedButton(onPressed: () {}, child: Text(MyText.createAccount)),
       ],
     );
   }
 }
 
-class LoginHeader extends StatelessWidget {
-  const LoginHeader({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        //Title
-        Text(
-          MyText.loginTitle,
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        SizedBox(height: MySizes.sm),
-        //subtitle
-        Text(
-          MyText.loginSubTitle,
-          style: Theme.of(context).textTheme.bodySmall,
-        ),
-      ],
-    );
-  }
-}
---
+++ lib/features/authentication/screens/login/widgets/login_header.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/constant/sizes.dart';
+import '../../../../../utils/constant/text.dart';
+
+class MyLoginHeader extends StatelessWidget {
+  const MyLoginHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //Title
+        Text(
+          MyText.loginTitle,
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        SizedBox(height: MySizes.sm),
+        //subtitle
+        Text(
+          MyText.loginSubTitle,
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+      ],
+    );
+  }
+}
