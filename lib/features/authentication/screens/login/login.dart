2df6c364970c702fea5996d8e4190c7bc045cdf9
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-08-12 12:24:53 +0500

    complete login page frontend

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:shopping_app/common/style/padding.dart';
-import 'package:shopping_app/common/widget/button/elevated_button.dart';
 import 'package:shopping_app/features/authentication/screens/login/widgets/login_form.dart';
 import 'package:shopping_app/features/authentication/screens/login/widgets/login_header.dart';
-import 'package:shopping_app/utils/constant/colors.dart';
-import 'package:shopping_app/utils/constant/images.dart';
 import 'package:shopping_app/utils/constant/sizes.dart';
 import 'package:shopping_app/utils/constant/text.dart';
 
