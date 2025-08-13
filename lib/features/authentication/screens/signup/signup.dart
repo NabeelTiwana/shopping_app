3ffc7ece1f3cbe3556c5d348da67f990185338ce
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-08-13 14:19:41 +0500

    update signup page

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:shopping_app/common/style/padding.dart';
+import 'package:shopping_app/utils/constant/sizes.dart';
+import 'package:shopping_app/utils/constant/text.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
@@ -7,6 +11,68 @@ class SignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:MyPadding.screenPadding,
+          child: Column(
+            children: [
+              //Header
+              Text(MyText.signupTitle,style: Theme.of(context).textTheme.headlineMedium,),
+              SizedBox(height: MySizes.spaceBtwSections,),
+              //Form
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: MyText.firstName,
+                            prefixIcon: Icon(Iconsax.user),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: MySizes.spaceBtwInputFields,),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: MyText.lastName,
+                            prefixIcon: Icon(Iconsax.user),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: MySizes.spaceBtwInputFields,),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: MyText.email,
+                      prefixIcon: Icon(Iconsax.direct_right),
+                    ),
+                  ),
+                  SizedBox(height: MySizes.spaceBtwInputFields,),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: MyText.phoneNumber,
+                      prefixIcon: Icon(Iconsax.call),
+                    ),
+                  ),
+                  SizedBox(height: MySizes.spaceBtwInputFields,),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: MyText.password,
+                      prefixIcon: Icon(Iconsax.password_check),
+                    ),
+                  ),
+                ],
+              ),
+              //Divider
+              //Footer
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
