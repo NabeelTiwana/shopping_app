e50ac5f4e673fdc0f70c8a33dbf3d87b00a86402
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-08-13 16:09:40 +0500

    add images

--- lib/features/authentication/screens/forget_password/forget_password.dart
+++ lib/features/authentication/screens/forget_password/forget_password.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:shopping_app/common/style/padding.dart';
+import 'package:shopping_app/common/widget/button/elevated_button.dart';
+import 'package:shopping_app/utils/constant/sizes.dart';
+import 'package:shopping_app/utils/constant/text.dart';
 
 class ForgetPasswordScreen extends StatelessWidget {
   const ForgetPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: MyPadding.screenPadding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Header
+              //Title
+              Text(MyText.forgetPasswordTitle,style: Theme.of(context).textTheme.headlineMedium,),
+              SizedBox(height: MySizes.spaceBtwItems/2,),
+              //SUbTitle
+              Text(MyText.forgetPasswordSubTitle,style: Theme.of(context).textTheme.labelMedium,),
+              SizedBox(height: MySizes.spaceBtwSections*2.5,),
+              //Form
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: MyText.email,
+                  prefixIcon: Icon(Iconsax.direct_right),
+                ),
+              ),
+              SizedBox(height: MySizes.spaceBtwItems,),
+              //
+              MyElevatedButton(onPressed: (){}, child: Text(MyText.submit)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
