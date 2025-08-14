b942ab98f2d4effab64577c5356a67d8c670575d
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-08-14 17:45:33 +0500

    update buttom Navigation

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shopping_app/utils/constant/colors.dart';
 
@@ -7,24 +9,33 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int selectedIndex = 0;
+    final controller=Get.put(NavigationController());
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        elevation: 0,
-        selectedIndex: selectedIndex,
-        backgroundColor: MyColor.light,
-        indicatorColor: MyColor.black.withValues(alpha: 0.1),
-        onDestinationSelected: (index) {
-          selectedIndex = index;
-        },
+      body:Obx(()=> controller.screens[controller.selectedIndex.value],),
+      bottomNavigationBar: Obx(
+        ()=> NavigationBar(
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          backgroundColor: MyColor.light,
+          indicatorColor: MyColor.black.withValues(alpha: 0.1),
+          onDestinationSelected: (index) {
+            controller.selectedIndex.value = index;
+          },
 
-        destinations: [
-          NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
-          NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
-          NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
-        ],
+          destinations: [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
+        ),
       ),
     );
   }
+}
+class NavigationController extends GetxController{
+  RxInt  selectedIndex = 0.obs;
+
+  List<Widget>screens=[Container(color:Colors.red),];
+
 }
