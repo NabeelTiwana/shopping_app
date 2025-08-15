8a7caaff1fcffb004aad9bc4e0c1e253bfe96e61
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-08-15 15:25:23 +0500

    create edge container

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_app/features/shop/screens/home/widgets/primary_header_container.dart';
-import 'package:shopping_app/utils/constant/colors.dart';
-import 'package:shopping_app/utils/helpers/device_helper.dart';
 
-import '../../../../common/widget/custom_shape/circular_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MyPrimaryHeaderContainer(),
+      body: MyPrimaryHeaderContainer(child: Container(),),
     );
   }
 }
